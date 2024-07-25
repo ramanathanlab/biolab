c1ece3d48af8e3a4e783bf2c73da4631f7621c35
Kyle Hippe <kyle.hippe@gmail.com>
2024-07-25 10:42:39 -0700

    Adding support for ESM3

--- biolab/modeling/models/esm.py
+++ biolab/modeling/models/esm.py
@@ -150,3 +150,140 @@ class ESM(LM):
     def generate_sequences(self, input: list[str]) -> list[SequenceModelOutput]:
         """Generate sequences from one or more input prompts"""
         raise NotImplementedError
+
+
+class ESM3Config(LMConfig):
+
+    name: Literal["ESM3"] = "ESM3"
+    # Model id or path to load the model
+    pretrained_model_name_or_path: str = "esm3_sm_open_v1"
+    # HF token with read only access to ESM3
+    hf_token: str
+    # path to HF cache if download needed
+    cache_dir: Optional[str] = None
+    # Use the model in half precision
+    half_precision: bool = False
+    # Set the model to evaluation mode
+    eval_mode: bool = True
+
+
+@model_registry.register(config=ESM3Config)
+class ESM3(LM):
+
+    model_input = "aminoacid"
+    model_encoding = "char"
+
+    def __init__(self, config: ESM3Config) -> None:
+        import os
+
+        os.environ["HF_HOME"] = config.cache_dir
+        os.environ["HF_TOKEN"] = config.hf_token
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+        from esm.models.esm3 import ESM3
+        from esm.tokenization.sequence_tokenizer import EsmSequenceTokenizer
+
+        # Load the model
+        model = ESM3.from_pretrained(config.pretrained_model_name_or_path)
+
+        # Convert the model to half precision
+        # if config.half_precision:
+        #     model.half()
+
+        # Set the model to evaluation mode
+        if config.eval_mode:
+            model.eval()
+
+        # Load the model onto the device
+        device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu",
+        )
+        model.to(device)
+
+        # Load the tokenizer
+        tokenizer = EsmSequenceTokenizer()
+
+        # Set persistent attributes
+        self.config = config
+        self.model = model
+        self._tokenizer = tokenizer
+        self._device = device
+
+    @property
+    def tokenizer(self) -> PreTrainedTokenizer:
+        return self._tokenizer
+
+    @property
+    def tokenizer_config(self) -> dict[str, Any]:
+        return (
+            self.config.tokenizer_config.model_dump()
+            if self.config.tokenizer_config
+            else {}
+        )
+
+    @property
+    def dataloader_config(self) -> dict[str, Any]:
+        return (
+            self.config.dataloader_config.model_dump()
+            if self.config.dataloader_config
+            else {}
+        )
+
+    @property
+    def device(self) -> torch.device:
+        return self._device
+
+    def generate_embeddings(self, sequences: list[str]) -> list[SequenceModelOutput]:
+        """Generate embeddings and logits for sequence input."""
+
+        # Tokenize the dataset
+        # TODO: remove column specifier, is this a property of the LM?
+        def tokenize_input(examples):
+            return self.tokenizer(examples["sequences"], **self.tokenizer_config)
+
+        modeling_input = {"sequences": sequences}
+        modeling_dataset = Dataset.from_dict(modeling_input)
+        modeling_dataset = modeling_dataset.map(
+            tokenize_input,
+            batched=True,
+            remove_columns=["sequences"],
+        ).with_format("torch")
+
+        # turn into dataloader and grab dset info
+        dataloader = DataLoader(modeling_dataset, **self.dataloader_config)
+
+        # Generate embeddings
+        model_outputs: list[SequenceModelOutput] = []
+        with torch.no_grad():
+            with logging_redirect_tqdm(loggers=[logger]):
+                for batch in tqdm(dataloader, desc="Generating embeddings"):
+                    # The model takes lots of types of inputs in different tracks
+                    # Until we can support non-sequence types the only thing we need is the input_ids
+
+                    outputs = self.model(
+                        sequence_tokens=batch["input_ids"].to(self.device)
+                    )
+
+                    # Get the sequence lengths  bos/eos in esm model, remove last token)
+                    seq_lengths = batch["attention_mask"].sum(axis=1) - 1
+
+                    # Get the last hidden state
+                    last_hidden_state = outputs.embeddings
+
+                    # Move the outputs to the CPU
+                    embedding = last_hidden_state.cpu().detach().numpy()
+
+                    # Create the output objects
+                    for i, seq_len in enumerate(seq_lengths):
+                        # Remove the bos token and the padding
+                        trimmed_embedding = embedding[i, 1:seq_len, :]
+
+                        # Create the output object
+                        output = SequenceModelOutput(embedding=trimmed_embedding)
+                        model_outputs.append(output)
+
+        return model_outputs
+
+    def generate_sequences(self, input: list[str]) -> list[SequenceModelOutput]:
+        """Generate sequences from one or more input prompts"""
+        raise NotImplementedError
