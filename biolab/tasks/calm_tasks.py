74ce5e1eb5e4e9293295b27ab209416c71c92718
braceal <abrace05@gmail.com>
2024-09-09 15:20:00 -0500

    calm task refactor. auto download task data. calm solubility task.

--- biolab/tasks/calm_tasks.py
+++ biolab/tasks/calm_tasks.py
@@ -1,8 +1,25 @@
-from __future__ import annotations  # noqa: D100
+"""Tasks for CaLM."""
 
+from __future__ import annotations
+
+import os
+import subprocess
+import sys
+from abc import ABC
+from pathlib import Path
 from typing import Literal
 
 import datasets
+import pandas as pd
+from Bio.Seq import Seq
+from pydantic import Field
+from pydantic import field_validator
+from pydantic import model_validator
+
+if sys.version_info >= (3, 11):  # pragma: >=3.11 cover
+    from typing import Self
+else:  # pragma: <3.11 cover
+    from typing_extensions import Self
 
 from biolab import metric_registry
 from biolab import task_registry
@@ -15,29 +32,117 @@ from biolab.tasks.core.utils import find_transformation
 from biolab.tasks.core.utils import limit_training_samples
 
 
-class CaLMMeltomeConfig(TaskConfig):
-    """Configuration for MDH GC content. (Debug task)."""
+def _write_sequence_dset(
+    sequences: list[Seq], labels: list[float | str], output_path: Path
+):
+    """Write a sequence dataset to disk."""
+    # Check that the sequences and labels are the same length
+    if len(sequences) != len(labels):
+        raise ValueError('Sequences and labels must be the same length')
 
-    # Name of the task
-    name: Literal['CaLMMeltome'] = 'CaLMMeltome'
-    # Metrics to measure TODO: should be choice of literals
-    metrics: list[str] = ['mse', 'r2', 'pearson']
+    # Setup the data fields
+    data = {
+        'dna': [str(x).upper() for x in sequences],
+        'aminoacid': [str(x.translate()) for x in sequences],
+        'label': labels,
+    }
 
-    # Whether to limit the number of training samples
-    max_samples: int | None = None
+    # Create the dataset
+    dset = datasets.Dataset.from_dict(data)
 
-    # Dataset column to look for labels:
-    target_col: str = 'label'
+    # Save the dataset to disk
+    dset.save_to_disk(output_path)
+
+
+def _setup_calm_task(
+    csv_path: Path, output_path: Path, sequence_col: str, label_col: str
+):
+    """Process the CaLM meltome CSV file."""
+    # Load the CSV file and create sequences and labels
+    df = pd.read_csv(csv_path, header=0)
+    sequences = [Seq(seq) for seq in df[sequence_col]]
+    labels = df[label_col].to_list()
+
+    # Write the dataset to disk
+    _write_sequence_dset(sequences, labels, output_path)
+
+
+def download_calm_tasks(download_dir: Path) -> None:
+    """Download and process the CaLM tasks."""
+    # The CaLM repository path
+    repo_path = 'https://github.com/oxpig/CaLM/tree/main'
+
+    # Clone the repository
+    subprocess.run(['git', 'clone', repo_path], check=True, cwd=download_dir)
+
+    # Set the data path
+    calm_data_root = download_dir / 'CaLM' / 'data'
+
+    # Process the meltome task data
+    input_path = calm_data_root / 'meltome' / 'meltome_data.csv'
+    output_path = download_dir / CaLMMeltomeConfig.name
+    _setup_calm_task(input_path, output_path, 'sequence', 'melting_temperature')
+
+    # Process the solubility task data
+    input_path = calm_data_root / 'solubility' / 'solubility_data.csv'
+    output_path = download_dir / CaLMSolubilityConfig.name
+    _setup_calm_task(input_path, output_path, 'sequence', 'solubility')
+
+    # Remove the cloned repository
+    os.rmdir(download_dir / 'CaLM')
+
+
+class CaLMTaskConfig(TaskConfig, ABC):
+    """Configuration for CaLM tasks."""
+
+    # Placeholder for the task name (to be set by subclasses)
+    name: Literal[''] = ''
+
+    metrics: list[str] = Field(
+        default=['mse', 'r2', 'pearson'], description='Metrics to measure'
+    )
+    max_samples: int | None = Field(
+        default=None, description='Whether to limit the number of training samples'
+    )
+    target_col: str = Field(default='label', description='Target column in the dataset')
+
+    # TODO: Consider moving this and the validator to a more general location
+    download_dir: Path = Field(
+        default=Path.home() / '.biolab' / 'data',
+        description='Directory to download data',
+    )
+
+    # TODO: We can remove this if dataset_name_or_path is refactored
+    # to be compatible with auto downloads (this is so users don't have
+    # to manually set the data path)
+    dataset_name_or_path: str = ''
+
+    @model_validator(mode='after')
+    def _set_dataset(self) -> Self:
+        self.dataset_name_or_path = str(self.download_dir / self.name)
+        return self
+
+    @field_validator('download_dir')
+    @classmethod
+    def create_download_dir(cls, v: Path) -> Path:
+        """Create the download directory if it doesn't exist."""
+        v.mkdir(parents=True, exist_ok=True)
+        return v
+
+    @model_validator(mode='after')
+    def download(self) -> Self:
+        """Download the CaLM data."""
+        download_calm_tasks(self.download_dir)
+        return self
 
 
 # TODO: add caching to task (way to store some results/models/intermediates)
-@task_registry.register(config=CaLMMeltomeConfig)
-class CaLMMeltome(Task):
+class CaLMTask(Task, ABC):
     """Melting temperature prediction from diverse sequences."""
 
     resolution: str = 'sequence'
 
-    def __init__(self, config: CaLMMeltomeConfig):
+    def __init__(self, config: CaLMTaskConfig):
         self.config = config
 
     def evaluate(self, model: LM):
@@ -91,3 +196,25 @@ class CaLMMeltome(Task):
 
         for metric in metrics:
             logger.info(f'Metric: {metric.__class__.__name__}\tValue: {metric.result}')
+
+
+class CaLMMeltomeConfig(CaLMTaskConfig):
+    """Configuration for CaLM meltome task."""
+
+    name: Literal['CaLMMeltome'] = 'CaLMMeltome'
+
+
+@task_registry.register(config=CaLMMeltomeConfig)
+class CaLMMeltome(CaLMTask):
+    """CaLM meltome (melting temperature) task."""
+
+
+class CaLMSolubilityConfig(CaLMTaskConfig):
+    """Configuration for CaLM solubility task."""
+
+    name: Literal['CaLMSolubility'] = 'CaLMSolubility'
+
+
+@task_registry.register(config=CaLMSolubilityConfig)
+class CaLMSolubility(CaLMTask):
+    """CaLM solubility task."""
